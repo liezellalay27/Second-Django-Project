753c7a0a8889fc0cb05bbf9142ea5c9e67812b9f
Liezel <liezellalay@gmail.com>
2025-09-22 00:59:39 +0800

    feat: complete app setup

--- Snapfolio/urls.py
+++ Snapfolio/urls.py
@@ -2,8 +2,14 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static  # Import this line
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('Snapfolio.urls')), # This connects the app
+    path('', include('Snapfolio.urls')),
 ]
+
+# This is only for the development server
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
